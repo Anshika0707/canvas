9093a4a7869fb8e41efa39428236b07c9e1960f5
Anshika0707 <83493340+Anshika0707@users.noreply.github.com>
2021-07-05 13:50:38 +0530

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -1,10 +1,10 @@
-canvas = document.getElementById("mycanvas");
+canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 color = "red";
 ctx.beginPath();
 ctx.strokeStyle = color;
 ctx.lineWidth = 2;
-ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
 ctx.stroke();
 
 canvas.addEventListener("mousedown ", my_mousedown);
@@ -24,6 +24,6 @@ function circle(mouse_x, mouse_y) {
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.lineWidth = 2;
-    ctx.arc(mouse_x, mouse_y, 40, 0, 2 * Math.PI);
+    ctx.arc(mouse_x, mouse_y, 40, 0, 2*Math.PI);
     ctx.stroke();
 }
